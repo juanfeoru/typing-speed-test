957a1aa5a682495b82cdc58cd3700b9df00d8fa1
juanfeoru <felipeortega383@gmail.com>
2026-02-17 17:35:45 -0500

    feat: Implement core UI layout with Header, Info, Stats, and responsive controls, and integrate custom font.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
+import Header from './components/Header';
+import Info from './components/Info';
 
 function App() {
-
   return (
-    <>
+    <div className='bg-neutral-900 min-h-screen p-6'>
+      <Header />
 
-    </>
-  )
+      <Info />
+    </div>
+  );
 }
 
-export default App
+export default App;
